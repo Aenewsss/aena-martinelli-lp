19ca74cf7a99d70ed54c66d4ad3fd8ecf5db1176
aenewsss <aenamartinelli@gmail.com>
2023-07-04 23:51:00 -0300

    navbar created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Navbar from "@/components/Navbar"
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main >
+      <Navbar />
       <h1>DESPERTE O POTENCIAL DO SEU NEGÓCIO</h1>
     </main>
   )
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <div className="navbar d-flex align-center justify-between">
+            <img src="/images/logo-aena.svg" alt="Aenã Martinelli logo" />
+
+            <div className="d-flex gap-1">
+                <a href="#" className="text-green fs-24 nav-link nav-important">Quero saber mais!</a>
+                <a href="#" className="nav-link">Projetos</a>
+                <a href="#" className="nav-link">Sobre mim</a>
+                <a href="#" className="nav-link">Feedbacks</a>
+                <a href="#" className="nav-link">YouTube</a>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
