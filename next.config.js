bbb6654ff21c3a83f8783ee96b7c2c4961889124
aenewsss <aenamartinelli@gmail.com>
2023-07-16 15:25:24 -0300

    change next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
+        NEXT_PUBLIC_TEMPLATE_ID: process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        NEXT_PUBLIC_TEMPLATE_NEW_MESSAGE_ID: process.env.NEXT_PUBLIC_TEMPLATE_NEW_MESSAGE_ID,
+        NEXT_PUBLIC_USER_ID: process.env.NEXT_PUBLIC_USER_ID,
+    }
+}
 
 module.exports = nextConfig
