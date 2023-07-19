c9f35b4c09dac2f210abf34f3b5876dbc89bb63f
aenewsss <aenamartinelli@gmail.com>
2023-07-19 09:27:20 -0300

    create a separate component for carousel btns

--- src/components/Feedbacks.tsx
+++ src/components/Feedbacks.tsx
@@ -1,38 +1,15 @@
-"use client"
-
 import Image from "next/image";
+import CarouselButtons from "./CarouselButtons";
 
 const Feedbacks = () => {
-
-    function handleCarouselClick(e: any) {
-        const id = e.target.id;
-        const slides = document.querySelector("[data-slides]")!
-        const activeSlide = slides.querySelector("[data-active]")!
-
-        // verify if go to next or prev slide
-        let offset = id === "next" ? 1 : -1
-
-        // get new index from slides array
-        let newIndex = Array.from(slides.children || []).indexOf(activeSlide) + offset
-
-        // if < 0, new index will be the last, if >= length, new index will be the first
-        if (newIndex < 0) newIndex = slides.children.length - 1
-        if (newIndex >= slides.children.length) newIndex = 0
-
-        // insert new index slide to data-active and delete the current
-        const newSlide = slides.children[newIndex];
-        if (newSlide instanceof HTMLElement) newSlide.dataset.active = "true"
-
-        delete (activeSlide as HTMLElement).dataset.active
-    }
 
     return (
         <section id="feedbacks" className="container mt-md-5 p-4">
             <h2 className="text-center after-center">O que os clientes dizem sobre meus serviços</h2>
 
             <div className="carousel">
-                <button id="prev" onClick={handleCarouselClick} className="carousel-btn prev">&#8678;</button>
-                <button id="next" onClick={handleCarouselClick} className="carousel-btn next">&#8680;</button>
+
+                <CarouselButtons />
 
                 <div className="slides" data-slides>
                     <div className="slide" data-active>
