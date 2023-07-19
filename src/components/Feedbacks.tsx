36851e7bde318b455202fe4af6f39483934a08d0
aenewsss <aenamartinelli@gmail.com>
2023-07-19 09:11:08 -0300

    fixing faeedback errors

--- src/components/Feedbacks.tsx
+++ src/components/Feedbacks.tsx
@@ -1,28 +1,29 @@
 "use client"
 
 import Image from "next/image";
-import { useState } from "react";
 
 const Feedbacks = () => {
 
     function handleCarouselClick(e: any) {
         const id = e.target.id;
+        const slides = document.querySelector("[data-slides]")!
+        const activeSlide = slides.querySelector("[data-active]")!
 
+        // verify if go to next or prev slide
         let offset = id === "next" ? 1 : -1
 
-        const slides = document.querySelector("[data-slides]")
+        // get new index from slides array
+        let newIndex = Array.from(slides.children || []).indexOf(activeSlide) + offset
 
-        const activeSlide = slides?.querySelector("[data-active]")
+        // if < 0, new index will be the last, if >= length, new index will be the first
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
 
-        let newIndex = [...slides?.children].indexOf(activeSlide) + offset
+        // insert new index slide to data-active and delete the current
+        const newSlide = slides.children[newIndex];
+        if (newSlide instanceof HTMLElement) newSlide.dataset.active = "true"
 
-        if (newIndex < 0) newIndex = slides?.children.length - 1
-        if (newIndex >= slides?.children.length) newIndex = 0
-
-        slides.children[newIndex].dataset.active = true
-        delete activeSlide?.dataset.active
-
-        console.log(activeSlide.dataset)
+        delete (activeSlide as HTMLElement).dataset.active
     }
 
     return (
@@ -38,18 +39,18 @@ const Feedbacks = () => {
                         <div className="card-feedback">
                             <Image height={40} width={40} src="/icons/aspas.svg" alt="Aspas ícone" />
                             <div className="d-flex justify-between flex-wrap">
-                                <p className="mt-2 w-80 fs-24">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, inventore! Quos ipsum atque maiores rerum laborum? Architecto, sit aperiam illo voluptatum, obcaecati aliquam non </p>
-                                <Image className="rounded-circle  mt-md-0 mt-2" height={100} width={100} src="/images/topo-aena.png" alt="s" />
+                                <p className="mt-2 w-80 fs-24">Um cara super pró ativo que supera as expectativas! Tem o poder de resolver problemas de maneira rápida e eficiente! Super Recomendo</p>
+                                <Image className="rounded-circle  mt-md-0 mt-2" height={100} width={100} src="/feedbacks/rodrigo.jpg" alt="s" />
                             </div>
 
                         </div>
                     </div>
-                    <div className="slide">
+                    <div className="slide" >
                         <div className="card-feedback">
                             <Image height={40} width={40} src="/icons/aspas.svg" alt="Aspas ícone" />
                             <div className="d-flex justify-between flex-wrap">
                                 <p className="mt-2 w-80 fs-24">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, inventore! Quos ipsum atque maiores rerum laborum? Architecto, sit aperiam illo voluptatum, obcaecati aliquam non </p>
-                                <Image className="rounded-circle  mt-md-0 mt-2" height={100} width={100} src="https://images.unsplash.com/photo-1635249287052-f52cfe0a9f8c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80" alt="s" />
+                                <Image className="rounded-circle  mt-md-0 mt-2" height={100} width={100} src="/images/topo-aena.png" alt="s" />
                             </div>
 
                         </div>
