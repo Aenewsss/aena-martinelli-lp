7f99c47ffc6fe16ffec25a728571cc782841a6f5
aenewsss <aenamartinelli@gmail.com>
2023-08-09 16:23:37 -0300

    mesi feedback

--- src/components/Feedbacks.tsx
+++ src/components/Feedbacks.tsx
@@ -12,12 +12,20 @@ const Feedbacks = () => {
                 <CarouselButtons />
 
                 <div className="slides" data-slides>
+                  
                     <div className="slide" data-active>
                         <div className="card-feedback">
                             <Image height={40} width={40} src="/icons/aspas.svg" alt="Aspas ícone" />
-                            <div className="d-flex justify-between flex-wrap">
-                                <p className="mt-2 w-80 fs-24">Um cara super pró ativo que supera as expectativas! Tem o poder de resolver problemas de maneira rápida e eficiente! Super Recomendo</p>
-                                <Image className="rounded-circle  mt-md-0 mt-2" height={100} width={100} src="/feedbacks/rodrigo.jpg" alt="s" />
+                            <div className="d-flex justify-between flex-wrap align-center">
+                                <p className="mt-2 w-80 fs-24">
+                                    O trabalho desenvolvido pelo Aenã colocou a apresentação 
+                                    do meu serviço em outro patamar. Durante todo o processo, 
+                                    ele se mostrou comprometido e interessado em entregar o 
+                                    melhor resultado possível. O site desenvolvido por ele foi, 
+                                    sem dúvidas, um divisor de águas na minha carreira. Recomendo 
+                                    de olhos fechados!
+                                </p>
+                                <Image className="feedback-image rounded-circle  mt-md-0 mt-2" height={50} width={100} src="/feedbacks/mesi.jpg" alt="Feedback Lucas Mesiano" />
                             </div>
 
                         </div>
@@ -25,9 +33,9 @@ const Feedbacks = () => {
                     <div className="slide" >
                         <div className="card-feedback">
                             <Image height={40} width={40} src="/icons/aspas.svg" alt="Aspas ícone" />
-                            <div className="d-flex justify-between flex-wrap">
-                                <p className="mt-2 w-80 fs-24">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, inventore! Quos ipsum atque maiores rerum laborum? Architecto, sit aperiam illo voluptatum, obcaecati aliquam non </p>
-                                <Image className="rounded-circle  mt-md-0 mt-2" height={100} width={100} src="/images/topo-aena.png" alt="s" />
+                            <div className="d-flex justify-between flex-wrap align-center">
+                                <p className="mt-2 w-80 fs-24">Um cara super pró ativo que supera as expectativas! Tem o poder de resolver os problemas e demandas de maneira MUITO rápida e eficiente! Super Recomendo</p>
+                                <Image className="feedback-image rounded-circle  mt-md-0 mt-2" height={100} width={100} src="/feedbacks/rodrigo.jpg" alt="Feedback Rodrigo Abreu" />
                             </div>
 
                         </div>
@@ -37,7 +45,7 @@ const Feedbacks = () => {
                             <Image height={40} width={40} src="/icons/aspas.svg" alt="Aspas ícone" />
                             <div className="d-flex justify-between flex-wrap">
                                 <p className="mt-2 w-80 fs-24">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, inventore! Quos ipsum atque maiores rerum laborum? Architecto, sit aperiam illo voluptatum, obcaecati aliquam non </p>
-                                <Image className="rounded-circle  mt-md-0 mt-2" height={100} width={100} src="https://images.unsplash.com/photo-1686041607888-78b4d624ed6e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" alt="s" />
+                                <Image className="feedback-image rounded-circle  mt-md-0 mt-2" height={100} width={100} src="https://images.unsplash.com/photo-1686041607888-78b4d624ed6e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" alt="s" />
                             </div>
 
                         </div>
@@ -45,7 +53,7 @@ const Feedbacks = () => {
                 </div>
 
             </div>
-        </section>
+        </section >
     );
 }
 
