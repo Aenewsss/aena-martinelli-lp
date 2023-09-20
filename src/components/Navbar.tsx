12a805b8ab31ca1dcaba82217f066e013ed74b2d
Aenewsss <62079043+Aenewsss@users.noreply.github.com>
2023-09-20 15:30:03 -0300

    OK

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -36,25 +36,9 @@ const Navbar = () => {
                         className="text-green fs-24 nav-link nav-important">
                         Quero saber mais!
                     </a>
-                    <ul className="d-flex flex-wrap gap-1 justify-center nav-list desktop">
-                        <li><a href="#portfolio" className="nav-link">Projetos</a></li>
-                        <li><a href="#about" className="nav-link">Sobre mim</a></li>
-                        <li><a href="#feedbacks" className="nav-link">Feedbacks</a></li>
-                        <li><a href="#youtube" className="nav-link">YouTube</a></li>
-                    </ul>
                 </div>
 
             </div>
-            <input id="expand" type="checkbox" hidden />
-            <ul className="d-flex flex-wrap gap-1 justify-center nav-list nav-mobile">
-                <li><a href="#portfolio" className="nav-link">Projetos</a></li>
-                <li><a href="#about" className="nav-link">Sobre mim</a></li>
-                <li><a href="#feedbacks" className="nav-link">Feedbacks</a></li>
-                <li><a href="#youtube" className="nav-link">YouTube</a></li>
-            </ul>
-            <label htmlFor="expand" className="expand-button">
-                <Image width={40} height={40} src="/icons/menu-icon.svg" alt="ícone menu de expansão" />
-            </label>
         </div>
     );
 }
